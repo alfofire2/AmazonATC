923191a3ec123f0072903dfab3f1ccd30685588c
alfofire2 <77249734+alfofire2@users.noreply.github.com>
2025-01-23 00:55:02 +0100

    Create atc.js

---
+++ atc.js
@@ -0,0 +1,18 @@
+function btn(){
+    const item = document.getElementById("outOfStock");
+    if (item === null){
+        const err = "You have to run this script on Amazon's not available item page.\nThen the add to cart button will appear.";
+        alert(err);
+        throw new Error(err);
+    }
+    else{
+        const btn = document.createElement("button");
+        btn.setAttribute("href","#addToCart");
+        btn.setAttribute("style","background-color: #34a4eb !important; border-radius: 10px; border: 0px; padding: 10px;");
+        btn.setAttribute("id","addToCart");
+        btn.innerText = "t.me/alfo_daniello | Add to Cart\n\nClick here to add the item to your cart";
+        item.prepend(btn);
+    }
+}
+btn()
+
