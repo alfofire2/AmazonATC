a16069c2b7f0e06b2435c52c11d74853093a9c69
alfofire2 <77249734+alfofire2@users.noreply.github.com>
2025-01-23 19:09:36 +0100

    Update atcref.js

--- atcref.js
+++ atcref.js
@@ -6,21 +6,25 @@ function btn(){
         throw new Error(err);
     }
     else{
-        const refTag = "alfodaniell0d-21"; 
+        const refTag = "alfodaniell0d-21";
         const currentUrl = window.location.href;
-        const newUrl = currentUrl + (currentUrl.includes('?') ? '&' : '?') + 'ref=' + refTag;
-
-        window.location.href = newUrl;
-
-        window.onload = function() {
-            const btn = document.createElement("button");
-            btn.setAttribute("href","#addToCart");
-            btn.setAttribute("style","background-color: #34a4eb !important; border-radius: 10px; border: 0px; padding: 10px;");
-            btn.setAttribute("id","addToCart");
-            btn.innerText = "Add to Cart | t.me/alfo_daniello\n\nClick here to add the item to your cart";
-
-            item.prepend(btn);
-        };
+        
+        if (!currentUrl.includes('tag=')) {
+            const newUrl = currentUrl + (currentUrl.includes('?') ? '&' : '?') + 'ref=' + refTag;
+            window.location.href = newUrl;
+        } else {
+            createAddToCartButton(item);
+        }
     }
 }
-btn()
+
+function createAddToCartButton(item) {
+    const btn = document.createElement("button");
+    btn.setAttribute("href","#addToCart");
+    btn.setAttribute("style","background-color: #34a4eb !important; border-radius: 10px; border: 0px; padding: 10px;");
+    btn.setAttribute("id","addToCart");
+    btn.innerText = "Add to Cart | t.me/alfo_daniello\n\nClick here to add the item to your cart";
+    item.prepend(btn);
+}
+
+btn();
